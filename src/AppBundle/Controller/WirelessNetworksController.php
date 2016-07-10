ecdb25f48d5b19e2a67665c1de572d46292095d9
Raymon de Looff <raydelooff@gmail.com>
2016-07-10 21:57:19 +0200

    Add controller for the wireless networks view

---
+++ src/AppBundle/Controller/WirelessNetworksController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class WirelessNetworksController extends Controller
+{
+	/**
+     * @Route("/wireless-networks", name="wireless_networks")
+     */
+    public function indexAction(Request $request)
+    {
+    	$scanner = $this->get('app.wireless.scanner');
+
+        return $this->render('default/wireless-networks.html.twig', [
+        	'networks' => $scanner->getNetworks(),
+        ]);
+    }
+}
