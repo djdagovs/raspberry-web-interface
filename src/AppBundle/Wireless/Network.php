c7aa696d9b5a327b1dc0ae4bf955e3d0a7222c53
Raymon de Looff <raydelooff@gmail.com>
2016-07-11 19:59:25 +0200

    Add regex for saved network list row

--- src/AppBundle/Wireless/Network.php
+++ src/AppBundle/Wireless/Network.php
@@ -179,8 +179,13 @@ class Network
      */
     public static function fromRow($row)
     {
-        $row = preg_split('/[\t]+/', $row);
-        $network = new Network($row[0], $row[1], $row[2], $row[3]);
+        $row = preg_match('/(?P<id>[0-9]+)\s(?P<ssid>[a-zA-Z0-9 ]+)?\s(?P<bssid>[0-9a-zA-Z\:]+)\s(?P<flags>.*)/', $row, $matches);
+        $id = $matches['id'];
+        $ssid = isset($matches['ssid']) ? $matches['ssid'] : null;
+        $bssid = $matches['bssid'];
+        $flags = $matches['flags'];
+
+        $network = new Network($id, $ssid, $bssid, $flags);
 
         return $network;
     }
