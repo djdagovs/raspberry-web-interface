6d2af960766cb96149aad6b636a40044c12bc132
Raymon de Looff <raydelooff@gmail.com>
2016-07-11 15:00:27 +0200

    Add network manager service

---
+++ src/AppBundle/Wireless/Network.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace AppBundle\Wireless;
+
+class Network
+{
+    const STATUS_CURRENT = 'Current';
+    const STATUS_ENABLED = 'Enabled';
+    const STATUS_DISABLED = 'Disabled';
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $ssid;
+
+    /**
+     * @var string
+     */
+    private $bssid;
+
+    /**
+     * @var string
+     */
+    private $flags;
+
+    /**
+     * @var string
+     */
+    private $status;
+
+    public function __construct($id, $ssid, $bssid, $status)
+    {
+        $this->id = $id;
+        $this->ssid = $ssid;
+        $this->bssid = $bssid;
+        $this->flags = $flags;
+
+        // Parse flags
+        $this->parseFlags();
+    }
+
+    /**
+     * Gets the value of id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Gets the value of ssid.
+     *
+     * @return string
+     */
+    public function getSsid()
+    {
+        return $this->ssid;
+    }
+
+    /**
+     * Gets the value of bssid.
+     *
+     * @return string
+     */
+    public function getBssid()
+    {
+        return $this->bssid;
+    }
+
+    /**
+     * Gets the value of flags.
+     *
+     * @return string
+     */
+    public function getFlags()
+    {
+        return $this->flags;
+    }
+
+    /**
+     * Gets the value of status.
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Returns true if the network is the current network.
+     *
+     * @return boole True if the network is the current network.
+     */
+    public function isCurrent()
+    {
+        return $this->status == Network::STATUS_CURRENT;
+    }
+
+    /**
+     * Returns an array of network details.
+     *
+     * @return array The network details.
+     */
+    public function getDetails()
+    {
+        return [
+            'id' => $this->getId(),
+            'ssid' => $this->getSsid(),
+            'bssid' => $this->getBssid(),
+            'status' => $this->getStatus(),
+            'current' => $this->isCurrent(),
+        ];
+    }
+
+/**
+     * Checks if the network has the given flag.
+     *
+     * @param $flag string The flag to check.
+     * @return bool True if the network has the given flag, false otherwise.
+     */
+    private function hasFlag($flag)
+    {
+        preg_match('/(?P<current>\[CURRENT\])?(?P<enabled>\[ENABLED\])?(?P<disabled>\[DISABLED\])?/i', $this->flags, $matches);
+
+        return array_key_exists($flag, $matches) && !empty($matches[$flag]);
+    }
+
+    /**
+     * Checks if the network has the given array of flags.
+     *
+     * @param $flags array The array of flags to check.
+     * @return bool True if the network has the given flags, false otherwise.
+     */
+    private function hasFlags(array $flags)
+    {
+        foreach ($flags as $flag) {
+            if (!$this->hasFlag($flag)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Parses the network flags.
+     *
+     * @return void
+     */
+    public function parseFlags()
+    {
+        // Status
+        if ($this->hasFlag('current')) {
+            $this->status = Network::STATUS_CURRENT;
+        }
+
+        if ($this->hasFlag('enabled')) {
+            $this->status = Network::STATUS_ENABLED;
+        }
+
+        if ($this->hasFlag('disabled')) {
+            $this->status = Network::STATUS_DISABLED;
+        }
+    }
+
+    /**
+     * Make a network instance from the wpa_cli list_network results.
+     *
+     * @param string $row A network row from the cpa_cli list_network command.
+     * @return Network
+     */
+    public static function fromRow($row)
+    {
+        $row = preg_split('/[\t]+/', $row);
+        $network = new Network($row[0], $row[1], $row[2], $row[3]);
+
+        return $network;
+    }
+}
---
+++ src/AppBundle/Wireless/NetworkManager.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Wireless;
+
+use Psr\Log\LoggerInterface;
+use AppBundle\Command\Executor;
+use AppBundle\Network\NetworkInterface;
+use AppBundle\Exception\FileNotReadableException;
+
+class NetworkManager
+{
+    /**
+     * @var NetworkInterface
+     */
+    private $interface;
+
+    /**
+     * @var string
+     */
+    private $configurationFile;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var CommandExecutor
+     */
+    private $commandExecutor;
+
+    public function __construct(NetworkInterface $interface, $configurationFile, LoggerInterface $logger, Executor $commandExecutor)
+    {
+        $this->interface = $interface;
+        $this->setConfigurationFile($configurationFile);
+        $this->logger = $logger;
+        $this->commandExecutor = $commandExecutor;
+    }
+
+    /**
+     * Sets the configuration file used for the wpa_cli tool.
+     *
+     * @param string $configurationFile The path of the configuration file that is used by the wpa_cli tool.
+     */
+    public function setConfigurationFile($configurationFile)
+    {
+        if (!file_exists($configurationFile) || !is_readable($configurationFile)) {
+            throw new FileNotReadableException('The configuration file "'.$configurationFile.'" does not exist, or isn\'t readable.');
+        }
+
+        $this->configurationFile = $configurationFile;
+    }
+
+    /**
+     * Returns an array of saved networks.
+     *
+     * @return array An array of saved networks.
+     */
+    public function listNetworks()
+    {
+        $command = $this->commandExecutor->execute('wpa_cli -i '.$this->interface->getName().' list_networks');
+        $networks = array();
+
+        if ($command->isValid()) {
+            $output = $command->getOutput();
+
+            // Remove comment rows
+            for ($i = 0; $i < 2; $i++) {
+                array_shift($output);
+            }
+
+            foreach ($output as $row) {
+                $result = Network::fromRow($row);
+                $networks[] = $result->getDetails();
+            }
+
+            $this->logger->info('Found '.$command->getOutputCount().' saved networks.');
+        }
+
+        return $networks;
+    }
+}
