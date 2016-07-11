ab090514332dbb04da1923ecb05dde3b96f1fed6
Raymon de Looff <raydelooff@gmail.com>
2016-07-11 19:58:17 +0200

    Add connect network modal function

---
+++ web/assets/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    // Wireless networks connect button
+    var connectForm = $('#connect-modal-form');
+
+    $('.connect-button').click(function () {
+        var ssid = $(this).attr('data-ssid');
+        var bssid = $(this).attr('data-bssid');
+        var security = $(this).attr('data-security');
+        var keyManagement = $(this).attr('data-key-management');
+
+        connectForm.find('#ssid').val(ssid);
+        connectForm.find('#bssid').val(bssid);
+        connectForm.find('#security').val(security);
+        connectForm.find('#key-management').val(keyManagement);
+
+        if (security !== 'Open') {
+            connectForm.find('#connect-modal-form-password').hide();
+        } else {
+            connectForm.find('#connect-modal-form-password').show();
+        }
+    });
+
+    $('#connect-modal').on('shown.bs.modal', function () {
+        connectForm.find('#password').focus();
+    })
+
+});
